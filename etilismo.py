3718e4168fc737fe146b04ea1a24fe1901b929dd
sparban <estebanarbe55@gmail.com>
2022-12-06 19:42:04 -0500

    Agrego cruce de un punto

--- etilismo.py
+++ etilismo.py
@@ -9,7 +9,7 @@ token = "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"
 
 # Defino los individuos
 numIndividuos = 3
-numGenes = 2
+numGenes = 4
 rango = 600
 contEvaluacion = 0
 # funcion de evaluacion
@@ -27,35 +27,41 @@ def poblacion(numIndividuos, numGenes, rango):
     poblacion = []
     for i in range(0, numIndividuos):
         s= []
-        for j in range(0, (numGenes)):
+        for j in range(0, numGenes):
             s.append(random.uniform(-rango,rango)) 
-            #print(s)
         evaluacion = funcion_evaluacion(s, contEvaluacion)
         fitness = evaluacion[0]
         s.append(fitness)
         poblacion.append(s)
     return poblacion
 
-# Organizo la poblacion
-
 poblacioninicial = poblacion(numIndividuos, numGenes, rango)
-print(poblacioninicial)
+#print(poblacioninicial)
 
-'''
-def poblacion_organizada(poblacion, numIndividuos, numGenes):
-    vectorFitness = []
-    for i in range(0, numIndividuos):
-        vectorFitness.append(poblacion[i][numGenes])
-        #print(vectorFitness)
-    vectorFitness.sort(reverse=True)
-    for z in range(0,len(vectorFitness)):
-        for j in range(0, numIndividuos):
-            if poblacion[j] == vectorFitness[z]:
-                
-    return vectorFitness
-'''
 # Organizo los individuos de mayor a menor segun el fitness
 
 poblacionOrganizada = sorted(poblacioninicial, key=lambda fitness: fitness[numGenes], reverse=True)
 
-print(poblacionOrganizada)
+#------------------------ Cruce de un punto ---------------------------
+
+# Valor aleatorio
+C = random.randint(1, numGenes-1)
+print(C)
+
+listaPadres = poblacionOrganizada[0:2]
+padre1 = poblacionOrganizada[0][0:numGenes]
+padre2 = poblacionOrganizada[1][0:numGenes]
+
+print(f" lista de padres: {listaPadres}")
+print("padre 1: ", padre1)
+print("padre 2: ", padre2)
+
+hijo1 = padre1[0:C]
+hijo2 = padre2[0:C]
+
+hijo1.extend(padre2[C:len(padre2)])
+hijo2.extend(padre1[C:len(padre1)])
+
+print("Hijo 1:", hijo1)
+print("Hijo 2:", hijo2)
+
